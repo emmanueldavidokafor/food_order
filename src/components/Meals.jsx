d07df95832d40a476e85d382183841b9ac95e7c4
EmmaOkafor <okaforifebuche26@gmail.com>
2024-10-09 11:55:48 +0100

    Finished project

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useHttp from '../hooks/useHttp';
 import MealItem from './MealItem';
+import Error from './Error';
 
 const requestConfig = {};
 export default function Meals() {
@@ -13,7 +14,11 @@ export default function Meals() {
 	console.log(loadedMeals);
 
 	if (isLoading) {
-		return <p>Fetching meals...</p>;
+		return <p className='center'>Fetching meals...</p>;
+	}
+
+	if (error) {
+		return <Error title='Failed to fetch meals' message={error} />;
 	}
 
 	return (
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
 	items: [],
 	addItem: (item) => {},
 	removeItem: (id) => {},
+	clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -66,6 +67,10 @@ function cartReducer(state, action) {
 		return { ...state, items: updatedItems };
 	}
 
+	if (action.type === 'CLEAR_CART') {
+		return { ...state, items: [] };
+	}
+
 	return state;
 }
 
@@ -80,10 +85,15 @@ export function CartContextProvider({ children }) {
 		dispatchCartAction({ type: 'REMOVE_ITEM', id });
 	}
 
+	function clearCart() {
+		dispatchCartAction({ type: 'CLEAR_CART' });
+	}
+
 	const cartContext = {
 		items: cart.items,
 		addItem,
 		removeItem,
+		clearCart,
 	};
 	console.log(cartContext);
 
